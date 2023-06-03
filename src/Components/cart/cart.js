3ac49ba1d12be6d1f5226b769145807105b64ea4
Gabriel Ocampos <gabriel1234511@hotmail.com>
2023-06-03 01:17:26 -0300

    añado componentes

---
+++ src/Components/cart/cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Img from "../cart.jpg";
+
+export const cart = () => {
+  return (
+    <div className="cart">
+      <img src={Img} alt="carrito"></img>
+      <h2>1</h2>
+    </div>
+  );
+};
--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "../navbar/navbar.css";
 function NavPrueba() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Mi App</Navbar.Brand>
+      <Navbar.Brand href="#home">Tienda Cajón</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
